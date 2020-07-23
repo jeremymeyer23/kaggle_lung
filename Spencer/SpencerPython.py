f3cc4c9c432c460d5e688fafd85012ad4c446d5d
Spencer Ebert <spencer.r.ebert@gmail.com>
2020-07-23 16:17:12 -0600

    EDA Stuff

--- Spencer/SpencerPython.py
+++ Spencer/SpencerPython.py
@@ -5,8 +5,117 @@ Created on Thu Jul 23 14:14:18 2020
 @author: spenc
 """
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import pydicom
+import os
+import scipy.ndimage
 
-pd.read_csv()
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
+# Read in the picture data
+train = pd.read_csv("../DataKaggle/train.csv")
+test = pd.read_csv("../DataKaggle/test.csv")
+
+
+INPUT_FOLDER = '../DataKaggle/train/'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+# Load the scans in given folder path
+def load_scan(path):
+    slices = [pydicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
+    return slices
+
+# Change the hu of the slices to match the 
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+        
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+        
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+        image[slice_number] += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+first_patient = load_scan(INPUT_FOLDER + patients[0])
+first_patient_pixels = get_pixels_hu(first_patient)
+plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
+
+plt.imshow(first_patient_pixels[15], cmap=plt.cm.gray)
+plt.show()
+
+
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = np.array([scan[0].SliceThickness] + list(scan[0].PixelSpacing), dtype=np.float32)
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+    
+    return image, new_spacing
+
+pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1,1,1])
+print("Shape before resampling\t", first_patient_pixels.shape)
+print("Shape after resampling\t", pix_resampled.shape)
+
+plt.imshow(pix_resampled[150], cmap=plt.cm.gray)
+plt.show()
+
+def plot_3d(image, threshold=-300):
+    
+    # Position the scan upright, 
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2,1,0)
+    
+    verts, faces = measure.marching_cubes_classic(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+    
+plot_3d(pix_resampled, 400)
